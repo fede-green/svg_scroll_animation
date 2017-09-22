32b222c8fc363c338beaf883f5fb767f75f88b56
federico prato <federico.prato@siili.com>
2017-09-22 13:48:23 +0300

    fixed example js script

--- js/example.js
+++ js/example.js
@@ -1,42 +1,34 @@
 /**
  * @file
- * Provides JavaScript for this.
+ * An example of the JS you should insert in a medium to make the animation.
  */
 
-(function ($) {
+ function pathPrepare($el) {
+       var lineLength = $el[0].getTotalLength();
+       $el.css("stroke-dasharray", lineLength);
+       $el.css("stroke-dashoffset", lineLength);
+   }
 
-Drupal.behaviors.thisone = {
-  attach: function (context) {
+ function onload(){
+     var $word = jQuery("#siili");
 
-    function pathPrepare($el) {
-        var lineLength = $el[0].getTotalLength();
-        $el.css("stroke-dasharray", lineLength);
-        $el.css("stroke-dashoffset", lineLength);
-    }
+     // Prepare SVG.
+     pathPrepare($word);
 
-    function onload(){
-        var $word = $("#path3680");
+     // Init controller.
+     var controller = new ScrollMagic.Controller();
 
-        // Prepare SVG.
-        pathPrepare($word);
+     // Build tween.
+     var tween = new TimelineMax()
+         .add(TweenMax.to($word, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone}))
+         .add(TweenMax.to("path", 1, {stroke: "#FF5033", ease:Linear.easeNone}), 0);
 
-        // Init controller.
-        var controller = new ScrollMagic.Controller();
-
-        // Build tween.
-        var tween = new TimelineMax()
-            .add(TweenMax.to($word, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone}))
-            .add(TweenMax.to("path", 1, {stroke: "#33629c", ease:Linear.easeNone}), 0);
-
-        // Build scene.
-        var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 200, tweenChanges: true})
-            .setTween(tween)
-            .addIndicators()
-            .addTo(controller);
-    }
-
-    onload();
-  }
-};
-
-})(jQuery);
+     // Build scene.
+     var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 400, tweenChanges: true})
+         .setTween(tween)
+         .addIndicators()
+         .addTo(controller);
+ }
+ document.body.onload = function () {
+   onload();
+ }
